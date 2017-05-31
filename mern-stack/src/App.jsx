af845d5980811629523253f7a69e3e0e852b001c
Tosin <muyinatech@users.noreply.github.com>
2017-05-31 14:11:57 +0100

    Updated App.js

--- mern-stack/src/App.jsx
+++ mern-stack/src/App.jsx
@@ -1,4 +1,9 @@
-var component = <h1>Hello World!</h1>;
-var contentNode = document.getElementById('contents');
+const contentNode = document.getElementById('contents');
+
+const continents = ['Africa','America','Asia','Australia','Europe'];
+
+const message = continents.map(c => `Hello ${c}!`).join(' ');
+
+const component = <p>{message}</p>
 
 ReactDOM.render(component, contentNode);
--- mern-stack/static/App.js
+++ mern-stack/static/App.js
@@ -1,8 +1,17 @@
-var component = React.createElement(
-  'h1',
-  null,
-  'Hello World!'
-);
+'use strict';
+
 var contentNode = document.getElementById('contents');
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+
+var message = continents.map(function (c) {
+  return 'Hello ' + c + '!';
+}).join(' ');
+
+var component = React.createElement(
+  'p',
+  null,
+  message
+);
 
 ReactDOM.render(component, contentNode);
